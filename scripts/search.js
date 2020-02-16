c0ccd38a8d616cf38474e5986d2b0e03adcc73b5
ngp16 <ngp16@users.noreply.github.com>
2020-02-16 18:51:35 +0800

    applied search to all pages

--- scripts/search.js
+++ scripts/search.js
@@ -1,60 +1,82 @@
-var location = window.location.href;
-var url = new URL(location);
+var urlStr = window.location.href;
+var url = new URL(urlStr);
 
 var searchQuery = url.searchParams.get("q");
 
-var postsLink = new URL("../blog/posts.json", document.baseURI).href;
+//var postsLink = new URL("../blog/posts.json", document.baseURI).href;
+var postsLink = "https://ngp16.github.io/BlogSystemOnlineTest/blog/posts.json";
 
-var requestLink = `https://hssc-search-system.glitch.me/?q=${searchQuery}&postLink=${postsLink}`;
+postsLink = encodeURIComponent(postsLink);
 
-function failedConnect(response) {
-    if (!response.ok) {
-        throw Error(response.statusText);
-    }
-    return response.text();
-}
+var requestLink = `https://hssc-search-system.glitch.me/?q=${searchQuery}&postsLink=${postsLink}`;
 
 var template = document.getElementById("post-template");
-template.removeAttribute("id");
 
-fetch(requestLink, {
-        mode: 'cors'
-    })
-    .then(failedConnect)
-    .then(function (response) {
+getData();
 
-        var json = JSON.parse(response);
-        for (var post in json) {
+function getData() {
+    
+    fetch(requestLink, {
+            mode: 'cors'
+        })
+        .then (response => {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            return response.text();
+        })
+        .then(
+            (response) => {
 
-            var newItem = template.cloneNode(true);
-            setVal(newItem, "post-template-title", post.Title);
-            setVal(newItem, "post-template-author", post.Author);
-            setVal(newItem, "post-template-date", post.PublishTimeStr);
-            setVal(newItem, "post-template-preview", post.Preview);
-            setImg(newItem, "post-header-image-mobile", post.HeaderImageName, post.IsHeaderImageSet);
-            setImg(newItem, "post-header-image-desktop", post.HeaderImageName, post.IsHeaderImageSet);
-        }
+                console.log(response);
+                
+                var json = JSON.parse(response);
 
-        document.getElementById("search-results-container").appendChild(newItem);
+                for (var i = 0; i < json.length; i++) {
 
-    }).catch(function (error) {
+                    var post = json[i];
 
-    });
+                    var newItem = template.cloneNode(true);
+                    newItem.removeAttribute("id");
+
+                    setVal(newItem, "post-template-title", post.Title);
+                    setVal(newItem, "post-template-author", post.Author);
+                    setVal(newItem, "post-template-date", post.PublishTimeStr);
+                    setVal(newItem, "post-template-preview", post.Preview);
+                    setImg(newItem, "post-header-image-mobile", post.HeaderImageName, post.IsHeaderImageSet);
+                    setImg(newItem, "post-header-image-desktop", post.HeaderImageName, post.IsHeaderImageSet);
+
+                    document.getElementById("search-results-container").appendChild(newItem);
+
+                    newItem.setAttribute("href", `./blog/${post.HtmlFriendlyTitle}.html`);
+                }
+                
+
+            }
+        )
+        .catch(
+            (error) => {
+                alert(error.toString());
+                return;
+            }
+        );
+}
 
 function setVal(item, id, val) {
-    var obj = item.getElementById(id);
+    var obj = item.querySelector("#" + id);
     obj.innerHTML = val;
     obj.removeAttribute("id");
 }
 
 function setImg(item, id, val, hasImage) {
 
-    var obj = item.getElementById(id);
+    var obj = item.querySelector("#" + id);
 
     if (hasImage) {
         obj.setAttribute("src", `./content/images/${val}`);
-    }
-    else {
+    } else {
         obj.classList.add("hidden");
     }
+
+    obj.removeAttribute("id");
 }
