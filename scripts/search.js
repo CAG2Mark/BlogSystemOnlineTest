f64b4652eea902040febf71f4e4cd6873a07c799
ngp16 <ngp16@users.noreply.github.com>
2020-02-16 17:42:02 +0800

    search done

--- scripts/search.js
+++ scripts/search.js
@@ -25,9 +25,36 @@ fetch(requestLink, {
 
         var json = JSON.parse(response);
         for (var post in json) {
-            
+
+            var newItem = template.cloneNode(true);
+            setVal(newItem, "post-template-title", post.Title);
+            setVal(newItem, "post-template-author", post.Author);
+            setVal(newItem, "post-template-date", post.PublishTimeStr);
+            setVal(newItem, "post-template-preview", post.Preview);
+            setImg(newItem, "post-header-image-mobile", post.HeaderImageName, post.IsHeaderImageSet);
+            setImg(newItem, "post-header-image-desktop", post.HeaderImageName, post.IsHeaderImageSet);
         }
+
+        document.getElementById("search-results-container").appendChild(newItem);
 
     }).catch(function (error) {
 
     });
+
+function setVal(item, id, val) {
+    var obj = item.getElementById(id);
+    obj.innerHTML = val;
+    obj.removeAttribute("id");
+}
+
+function setImg(item, id, val, hasImage) {
+
+    var obj = item.getElementById(id);
+
+    if (hasImage) {
+        obj.setAttribute("src", `./content/images/${val}`);
+    }
+    else {
+        obj.classList.add("hidden");
+    }
+}
